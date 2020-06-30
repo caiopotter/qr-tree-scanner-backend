b9296cd01b8b5306f7a96c5d0ddf54a23899e7bd
caiopotter <limapotter@gmail.com>
2020-06-29 22:39:42 -0300

    Model de tree e user relacionados em belongsToMany

--- app/Tree.php
+++ app/Tree.php
@@ -9,4 +9,14 @@ class Tree extends Model
     protected $fillable = [
         'common_name', 'scientific_name', 'feature', 'origin', 'wood_type', 'utility'
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User', 'users_trees', 'tree_id', 'user_id');
+    }
+
+    public function code()
+    {
+        return $this->hasOne('App\QRCode');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function trees()
+    {
+        return $this->belongsToMany('App\Tree', 'users_trees', 'user_id', 'tree_id');
+    }
 }
